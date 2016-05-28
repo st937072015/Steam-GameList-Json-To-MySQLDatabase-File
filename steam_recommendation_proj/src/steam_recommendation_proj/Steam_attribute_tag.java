2831fa6ba9f451a59a6e6998e97ea48c364319f6
Rock Huang <st937072015@gmail.com>
2016-05-28 23:21:34 +0800

    steam attribute tag
    finish

--- steam_recommendation_proj/src/steam_recommendation_proj/Steam_attribute_tag.java
+++ steam_recommendation_proj/src/steam_recommendation_proj/Steam_attribute_tag.java
@@ -11,17 +11,14 @@ import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
-import org.openqa.jetty.html.Link;
+
 import org.openqa.selenium.By;
-import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import com.thoughtworks.selenium.Selenium;
-
-import org.openqa.selenium.WebElement;
 
 public class Steam_attribute_tag {
 
@@ -29,33 +26,68 @@ public class Steam_attribute_tag {
 
 	public static void main(String[] args) {
 		// 呼叫寫好的Mysql之function
-		Mysql_function gosql = new Mysql_function();
+		//Mysql_function gosql = new Mysql_function();
 		 
+		
+		
+		    // 開啟火狐瀏覽器
 		    WebDriver firefoxdrive = new FirefoxDriver();
-        
-			firefoxdrive.get("http://store.steampowered.com/app/271590/");
+            
+			firefoxdrive.get("http://store.steampowered.com/app/282350/");
 			
-			firefoxdrive.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+			
+	
+			
 			
 			// 選取介面語言點擊
 			firefoxdrive.findElement(By.id("language_pulldown")).click();
 			firefoxdrive.findElement(By.linkText("English（英文）")).click();
 			
-			WebDriverWait wait = new WebDriverWait(firefoxdrive,10);
+		   // 設定必要出現之網頁tag等待秒數警告
+		   WebDriverWait wait = new WebDriverWait(firefoxdrive,5);
 
            wait.until(ExpectedConditions.invisibilityOfElementWithText(By.xpath("//span[@id='language_pulldown']"), firefoxdrive.findElement(By.xpath("//span[@id='language_pulldown']")).getText()));
+           wait.until(ExpectedConditions.invisibilityOfElementLocated(By.xpath("//div[@class='app_tags popular_tags]")));
+           
+           // 點擊展開遊戲所屬之全部屬性標籤之清單
+           firefoxdrive.findElement(By.xpath("//div[@class='app_tag add_button']")).click();
+           
+           // 將全部找到的屬性tag網頁標籤內容全塞進List
+   		   List<WebElement> get_alltag = firefoxdrive.findElements(By.xpath("html/body/div/div/div/div/div/div/div/div/div[@class='app_tag_control popular']/a[@class='app_tag']"));
+   		
+   		   // 計算抓到的遊戲屬性標籤總數量
+   		int c = firefoxdrive.findElements(By.xpath("html/body/div/div/div/div/div/div/div/div/div[@class='app_tag_control popular']/a[@class='app_tag']")).size();
+   		
+   		System.out.println("一共有"+c+"個屬性標籤");
+   		
+		
+   		int alltag_count=0;
+   		
+   		// 提取遊戲的每個屬性標籤文字內容
+		for(WebElement  count :get_alltag){
+			alltag_count++;
+			System.out.println("第"+alltag_count+"個屬性標籤，"+"屬性為"+count.getText());
 			
-			System.out.println(firefoxdrive.findElement(By.xpath("//span[@id='language_pulldown']")).getText());
 			
-			System.out.println(firefoxdrive.findElement(By.xpath("//a[@class='app_tag']")).getText());
+			
+		}
+		
+           
+           
+           
+           
+			//System.out.println(firefoxdrive.findElement(By.xpath("//span[@id='language_pulldown']")).getText());
+			
+			//System.out.println(firefoxdrive.findElement(By.xpath("//a[@class='app_tag']")).getText());
 			
 			//int tag_count = firefoxdrive.findElement(By.xpath("")).size();
             //String get_tag_content = firefoxdrive.findElement(By.xpath("")).getText();
-			
+		
+			// 抓取完畢後關閉火狐瀏覽器
 			firefoxdrive.close();
 			
 			
-			System.out.println("");
+
 			
 			
 			/*
