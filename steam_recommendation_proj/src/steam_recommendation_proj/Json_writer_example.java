4e0748059c2bb3886cfdfb92a60f53fedf548aa5
Rock Huang <st937072015@gmail.com>
2016-05-30 00:36:29 +0800

    Steam_scraper fucntion test finish
    Steam_scraper fucntion test finish

---
+++ steam_recommendation_proj/src/steam_recommendation_proj/Json_writer_example.java
@@ -0,0 +1,19 @@
+package steam_recommendation_proj;
+
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+public class Json_writer_example {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
