e632e4fd48ace36a9471f8aa77d39b002e5825e5
Rock Huang <st937072015@gmail.com>
2016-07-17 03:28:37 +0800

    add Steam_review_personality_calculate function beta
    add Steam_review_personality_calculate function beta

---
+++ steam_recommendation_proj/src/steam_recommendation_proj/Control_hub16.java
@@ -0,0 +1,98 @@
+package steam_recommendation_proj;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.Set;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import org.openqa.selenium.remote.server.handler.MaximizeWindow;
+
+public class Control_hub16 {
+
+	public static void main(String[] args) {
+
+		try {
+
+					
+			
+			// 讀取advance字典檔
+        	FileReader advance_dictionary_read_json_reader = new FileReader("C:\\Users\\John-Wall\\Desktop\\Steam_review_dictionary\\Steam_user_review_dictionary_advance.json");
+        	JSONParser advance_dictionary_read_parser = new JSONParser();
+        	JSONObject advance_dictionary_read_object = (JSONObject) advance_dictionary_read_parser.parse(advance_dictionary_read_json_reader);
+
+        	
+        	JSONArray tfidf_dictionary_array = (JSONArray) advance_dictionary_read_object.get("all_advance_word");
+        	
+        	HashMap<String, ArrayList<ArrayList<Double>>> steam_review_advance_dictionary_hashmap = (HashMap<String, ArrayList<ArrayList<Double>>>)tfidf_dictionary_array.get(0);
+        	
+        	
+        	
+        	
+         
+        	
+        	/*
+        	
+        	for(Iterator iterator = steam_review_idf_dictionary_object.keySet().iterator(); iterator.hasNext();) {
+        		
+        		String key = (String) iterator.next();
+        	   
+        	    System.out.println("key:"+key+"value:"+(double)steam_review_idf_dictionary_object.get(key));
+        	    
+        	    
+        	    
+        	}
+			*/
+        	
+      
+        	
+
+			Steam_review_personality_calculate go_personality_tfidf = new Steam_review_personality_calculate();
+			
+			
+
+
+			for (int i = 1; i < 328973; i++) {
+				
+			
+           
+				
+				go_personality_tfidf.personality_tfidf_calculate(steam_review_advance_dictionary_hashmap, "C:\\Users\\John-Wall\\Desktop\\Steam_user_review_tfidf_join\\" + String.valueOf(i) + ".json", "steam_user_review_tfidf_join", String.valueOf(i), "C:\\Users\\John-Wall\\Desktop\\Steam_user_review_personality_tfidf_score\\", "steam_review_personality_tfidf");
+			    
+				
+
+
+			}
+
+			
+
+
+
+		} catch (
+
+		FileNotFoundException e) {
+			System.out.println(e.toString());
+		} catch (IOException e) {
+			System.out.println(e.toString());
+		} catch (ParseException e) {
+			System.out.println(e.toString());
+		} catch (NullPointerException e) {
+			System.out.println(e.toString());
+		}
+		
+		
+	}
+
+}
